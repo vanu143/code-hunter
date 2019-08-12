1bb154b66c65ca56247797315cdeb2872116cd30
vanu143 <54000504+vanu143@users.noreply.github.com>
2019-08-12 20:31:02 +0530

    Create Palindrome_Not.java

---
+++ guvi/set-2/qn12/Palindrome_Not.java
@@ -0,0 +1,19 @@
+import java.util.scanner;
+public class Palindrome_Not 
+{
+public static void main(String[] args) 
+{
+int num = 121, reversedInteger = 0, remainder, originalInteger;
+originalInteger = num;
+while( num != 0 )
+{
+remainder = num % 10;
+reversedInteger = reversedInteger * 10 + remainder;
+num  /= 10;
+}
+if (originalInteger == reversedInteger)
+System.out.println(originalInteger + " is a palindrome.");
+else
+System.out.println(originalInteger + " is not a palindrome.");
+}
+}
