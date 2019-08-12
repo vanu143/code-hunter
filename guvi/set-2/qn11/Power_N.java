67709a6039520a36ce189deb8270c304e1a1328a
vanu143 <54000504+vanu143@users.noreply.github.com>
2019-08-12 20:29:09 +0530

    Create Power_N.java

---
+++ guvi/set-2/qn11/Power_N.java
@@ -0,0 +1,16 @@
+import java.util.scanner;
+public class Power_N
+{
+public static void main(String[] args) 
+{
+int base = 3, exponent = 4;
+long result = 1;
+while (exponent != 0)
+{
+result *= base;
+--exponent;
+}
+System.out.println("Answer = " + result);
+}
+}
+© 2019 GitHub, Inc.
