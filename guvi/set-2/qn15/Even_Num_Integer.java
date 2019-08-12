51de50dc7f618bac64f67c979699e7b3b55781e6
vanu143 <54000504+vanu143@users.noreply.github.com>
2019-08-12 20:58:35 +0530

    Create Even_Num_Integer.java

---
+++ guvi/set-2/qn15/Even_Num_Integer.java
@@ -0,0 +1,22 @@
+import java.util.scanner;
+class Even_Num_Integer
+{
+public static void main(String[] args)
+{
+try
+{
+BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+System.out.println("Enter number : ");
+int num = Integer.parseInt(br1.readLine());
+System.out.println("Even Numbers:");
+for (int i=1;i <=num ; i++)
+{
+if(i%2==0 )
+{
+System.out.print(i+",");
+}
+}
+}
+catch(Exception e){}
+}
+}
