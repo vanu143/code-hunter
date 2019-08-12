fadaba62af5a39e565cf2df7a4e9df6ee12672b3
vanu143 <54000504+vanu143@users.noreply.github.com>
2019-08-12 11:02:07 +0530

    Create Vowel_Consonant.java

---
+++ guvi/set-1/qn3/Vowel_Consonant.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Vowel_Consonant 
+{
+public static void main(String args[])
+{
+System.out.println("Enter a character :");
+Scanner sc = new Scanner(System.in);
+char ch = sc.next().charAt(0);
+if(ch == 'a'|| ch == 'e'|| ch == 'i' ||ch == 'o' ||ch == 'u'||ch == ' ')
+{
+System.out.println("Given character is an vowel");
+}
+else
+{
+System.out.println("Given character is a consonant");
+}
+}
+}
